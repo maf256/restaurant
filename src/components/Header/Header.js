143d22f4b2ed8c5954b7baa8c630c9db83031958
maf256 <maf256@gmail.com>
2023-02-23 22:21:41 +0100

    create Header and Slider

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import Logofile from '../Images/Logo.png'
+
+
+export default function Header  () {
+  return (
+    <Logocontainer>
+      <h1>this is </h1>
+      <img src={Logofile} alt='logo'></img>
+    </Logocontainer>
+  )
+}
+
+const Logocontainer = styled.div`
+  width: 100%;
+  height: 180px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  
+  h1 {
+    visibility: hidden;
+    font-size: 2px;
+  }
+  
+  img {
+    width: 200px;
+    height: auto;
+  }
+
+`
---
+++ src/components/slider/slider.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import Slider1 from '../Images/slider1.jpg'
+import Slider2 from '../Images/slider2.jpg'
+import Slider3 from '../Images/slider3.jpg'
+import Slider4 from '../Images/slider4.jpg'
+
+
+const images = [
+    Slider1, Slider2, Slider3, Slider4
+  ];
+
+export default function App() {
+  const [value, setValue] = React.useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((v) => {
+        return v === 3 ? 0 : v + 1;
+      });
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Slidercontainer >
+        <Img url={images[value]} ></Img>
+    </Slidercontainer>
+  );
+}
+
+
+const Slidercontainer = styled.div`
+    width: 100%;
+    height: 77vh;
+    display: flex;
+    justify-content: center;
+`
+
+const Img = styled.div`
+    width: 80%;
+    height: auto;
+    background-image: url(${({ url }) => url });
+    transition: all 0.6s linear;
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: top;
+
+    @media (max-width: 1000px) {
+      width: 100%;
+  }
+
+
+`
